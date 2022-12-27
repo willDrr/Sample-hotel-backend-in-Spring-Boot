8bd950edbe28d7906f68052faa159b35e65e51b9
wild3r <willxdd97@gmail.com>
2022-12-27 06:33:47 -0600

    Adding initial project

---
+++ src/main/java/com/california/hotel/HotelApplication.java
@@ -0,0 +1,22 @@
+package com.california.hotel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelApplication implements CommandLineRunner{
+	private static final Logger logger = LoggerFactory.getLogger(HotelApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelApplication.class, args);
+		logger.debug("Application started succesfully");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
+}
