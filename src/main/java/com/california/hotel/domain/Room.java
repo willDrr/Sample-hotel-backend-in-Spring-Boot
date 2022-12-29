4e0e2f23c51f951c216c92b4017eded1305b9067
wild3r <willxdd97@gmail.com>
2022-12-28 20:45:55 -0600

    Adding Room domain

--- src/main/java/com/california/hotel/domain/Room.java
+++ src/main/java/com/california/hotel/domain/Room.java
@@ -2,37 +2,39 @@ package com.california.hotel.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
 public class Room {
-
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	private int roomNumber;
+	private int number;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "hotel_id")
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "hotel")
 	private Hotel hotel;
 
 	public Room() {
 
 	}
 
-	public Room(int roomNumber, Hotel hotel) {
-		this.roomNumber = roomNumber;
+	public Room(int number, Hotel hotel) {
+		this.number = number;
 		this.hotel = hotel;
 	}
 
-	public int getRoomNumber() {
-		return roomNumber;
+	public int getNumber() {
+		return number;
 	}
 
-	public void setRoomNumber(int roomNumber) {
-		this.roomNumber = roomNumber;
+	public void setNumber(int number) {
+		this.number = number;
 	}
 
 	public Hotel getHotel() {
